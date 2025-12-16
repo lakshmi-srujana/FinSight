fb9d29f46f134d204841c7e234c7c78404d77f39
lakshmi-srujana <pvlsrujana@gmail.com>
2025-12-16 11:54:34 +0530

    Add category-wise spending bar chart using Pandas

--- app.py
+++ app.py
@@ -22,10 +22,27 @@ st.title("FinSight Dashboard")
 if menu == "Dashboard":
     income = df[df["amount"] > 0]["amount"].sum()
     expenses = df[df["amount"] < 0]["amount"].sum()
+    balance = income - abs(expenses)
 
-    col1, col2 = st.columns(2)
-    col1.metric("Total Income", f"₹{income}")
-    col2.metric("Total Expenses", f"₹{abs(expenses)}")
+    col1, col2, col3 = st.columns(3)
+
+    col1.metric("💼 Income", f"₹{income}")
+    col2.metric("💸 Expenses", f"₹{abs(expenses)}")
+    col3.metric("💰 Balance", f"₹{balance}")
+
+    st.divider()
+    st.subheader("Overview")
+    st.write("Your financial summary based on recorded transactions.")
+    st.subheader("Spending by Category")
+    expense_df = df[df["amount"] < 0]
+    category_summary = (
+        expense_df.groupby("category")["amount"]
+        .sum()
+        .abs()
+    )
+
+    st.bar_chart(category_summary)
+
 
 elif menu == "Transactions":
     st.subheader("All Transactions")
